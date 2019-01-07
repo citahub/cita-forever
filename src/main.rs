102325ae55cf06429eb91dffa63035b91f598f4c
Boyu Yang <yangby@cryptape.com>
2019-01-07 23:20:35 +0800

    Upgrade default rust toolchain to stable.

--- src/main.rs
+++ src/main.rs
@@ -15,8 +15,6 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-#![feature(tool_lints)]
-
 extern crate clap;
 #[macro_use]
 extern crate logger;
